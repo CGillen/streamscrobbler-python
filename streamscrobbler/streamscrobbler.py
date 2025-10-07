a3f9140eb215569ee5b838b43008591ef3f8c79f
Corey Gillen <corey.j.gillen@gmail.com>
2025-10-06 20:34:07 -0700

    Convert to aiohttp for async network calls

--- streamscrobbler/streamscrobbler.py
+++ streamscrobbler/streamscrobbler.py
@@ -1,91 +1,85 @@
 # -*- coding: utf-8 -*-
 import re
-import urllib.request
-import urllib.error
 import urllib.parse
+from aiohttp import ClientSession
 
 
 # this is the function you should call with the url to get all data sorted as a object in the return
-def get_server_info(url):
+async def get_server_info(url):
     if urllib.parse.urlparse(url).path.endswith(".pls"):
-        address = check_pls(url)
+        address = await check_pls(url)
     else:
         address = url
     if isinstance(address, str):
-        meta_interval = get_all_data(address)
+        meta_interval = await get_all_data(address)
     else:
         meta_interval = {"status": 0, "metadata": None}
 
     return meta_interval
 
 
-def get_all_data(address):
+async def get_all_data(address):
     status = 0
 
-    request = urllib.request.Request(address)
     user_agent = "iTunes/9.1.1"
-    request.add_header("User-Agent", user_agent)
-    request.add_header("icy-metadata", 1)
+    headers = {
+        "User-Agent": user_agent,
+        "icy-metadata": "1"
+    }
+
+
     try:
-        response = urllib.request.urlopen(request, timeout=6)
-        headers = dict(response.info())
-        # Headers are case-insensitive
-        headers = {key.lower(): value for key, value in headers.items()}
+        async with ClientSession(headers=headers) as session:
+            async with session.get(address) as response:
+                headers = dict(response.headers)
+                # Headers are case-insensitive
+                headers = {key.lower(): value for key, value in headers.items()}
 
-        if "server" in headers:
-            shoutcast = headers["server"]
-        elif "x-powered-by" in headers:
-            shoutcast = headers["x-powered-by"]
-        elif "icy-notice1" in headers:
-            shoutcast = headers["icy-notice2"]
-        else:
-            shoutcast = True
+                if "server" in headers:
+                    shoutcast = headers["server"]
+                elif "x-powered-by" in headers:
+                    shoutcast = headers["x-powered-by"]
+                elif "icy-notice1" in headers:
+                    shoutcast = headers["icy-notice2"]
+                else:
+                    shoutcast = True
 
-        if isinstance(shoutcast, bool) and shoutcast:
-            status = 1
-            metadata = shoutcast_check(response, headers, True)
-        elif "SHOUTcast" in shoutcast:
-            status = 1
-            metadata = shoutcast_check(response, headers, False)
-        elif "Icecast" or "137" or "StreamMachine" in shoutcast:
-            status = 1
-            metadata = shoutcast_check(response, headers, True)
-        else:
-            metadata = False
-        response.close()
-        return {"status": status, "metadata": metadata}
-
-    except urllib.error.HTTPError as e:
-        print(("    Error, HTTPError = " + str(e.code)))
-        return {"status": status, "metadata": None}
-
-    except urllib.error.URLError as e:
-        print(("    Error, URLError: " + str(e.reason)))
-        return {"status": status, "metadata": None}
+                if isinstance(shoutcast, bool) and shoutcast:
+                    status = 1
+                    metadata = await shoutcast_check(response, headers, True)
+                elif "SHOUTcast" in shoutcast:
+                    status = 1
+                    metadata = await shoutcast_check(response, headers, False)
+                elif "Icecast" or "137" or "StreamMachine" in shoutcast:
+                    status = 1
+                    metadata = await shoutcast_check(response, headers, True)
+                else:
+                    metadata = False
+                return {"status": status, "metadata": metadata}
 
     except Exception as err:
         print(("    Error: " + str(err)))
         return {"status": status, "metadata": None}
 
 
-def check_pls(address):
+async def check_pls(address):
     try:
-        stream = None
-        response = urllib.request.urlopen(address, timeout=2)
-        for line in response:
-            if line.startswith(b"File1="):
-                stream = line.decode()
+        async with ClientSession() as session:
+            async with session.get(address) as response:
+                stream = None
+                for line in response.content:
+                    if line.startswith(b"File1="):
+                        stream = line.decode()
 
-        response.close()
-        if stream:
-            return stream[6:].strip("\n")
-        else:
-            return False
+                if stream:
+                    return stream[6:].strip("\n")
+                else:
+                    return False
     except Exception:
         return False
 
 
-def shoutcast_check(response, headers, is_old):
+async def shoutcast_check(response, headers, is_old):
     bitrate = None
     contenttype = None
 
@@ -111,7 +105,7 @@ def shoutcast_check(response, headers, is_old):
         # Maximum metadata frame size is 255*16=4080
         # Total buffer = music frame (metaint) + 1 byte (metadata length) + 4080 (255*16)
         read_buffer = metaint + 4081
-        content = response.read(read_buffer)
+        content = await response.content.readexactly(read_buffer)
         # Metadata true end is music frame + 1 byte + 16 * first byte after music frame
         metadata_end = metaint + 1 + int.from_bytes(content[metaint:metaint+1]) * 16
 
